ada4f431e31893d6b51e5b90aa110ddea4dd2c63
EngBeshoo <bghazr@gmail.com>
2026-08-29 18:09:18 +0300

    m

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          ui: ['@heroui/react', 'framer-motion'],
+        },
+      },
+    },
+  },
 })
